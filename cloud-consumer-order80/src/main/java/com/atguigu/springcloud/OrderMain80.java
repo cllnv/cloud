e183900661d0558a5c3251f6960535d782328262
cll <821266842@qq.com>
2020-10-16 11:20:49 +0800

    初始化初始化初始化初始化

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author chengpunan
+ * @Description //TODO $
+ * @Date $ $
+ * @Param $
+ * @return $
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
