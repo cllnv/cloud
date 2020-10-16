3dea5098e9dd5dbc6d31d3423fa2a9893ed03795
cll <821266842@qq.com>
2020-10-16 14:03:52 +0800

    初始化初始化初始化初始化

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommonResult<T> {  //泛型：如果装的payment 返回payment,装的order 返回order
 
-    private Integer code ;
+    private Integer code;
     private String message;
     private T data;
 
