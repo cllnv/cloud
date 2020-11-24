b8f3f987f5d2058511370a503c4bb24c3d1bd461
cll <821266842@qq.com>
2020-11-24 10:32:47 +0800

    啦啦啦啦啦啦啦啦绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -3,7 +3,6 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
-import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
@@ -17,8 +16,6 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @Slf4j
 public class PaymentController {
-    @Resource
-    private PaymentService paymentService;
 
     @Value("${server.port}")
     private String serverPort;
@@ -41,7 +38,9 @@ public class PaymentController {
     //只传给前端CommonResult，不需要前端了解其他的组件
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
-        int result = paymentService.create(payment);
+//        int result = paymentService.create(payment);
+
+        int result = 9;
         log.info("*****插入结果："+result);
         if(result > 0){
             return new CommonResult(200,"插入数据成功,serverPort:"+serverPort,result);
