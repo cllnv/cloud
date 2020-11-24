615943d3c83a2361dd0dbf409121b23ee633aed7
cll <821266842@qq.com>
2020-11-24 10:35:23 +0800

    啦啦啦啦啦啦啦啦绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿绿

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/controller.java
@@ -0,0 +1,56 @@
+package com.atguigu.springcloud.controller;
+
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class controller {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+
+    //只传给前端CommonResult，不需要前端了解其他的组件
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+//        int result = paymentService.create(payment);
+
+        int result = 9;
+        if(result > 0){
+            return new CommonResult(200,"插入数据成功,serverPort:"+serverPort,result);
+        }else{
+            return new CommonResult(444,"插入数据失败",null);
+        }
+    }
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+//        Payment payment = paymentService.getPaymentById(id);
+//        log.info("*****插入结果："+payment);
+//        if(payment != null){
+        CommonResult m = new CommonResult(200,"查询成功,serverPort:"+serverPort,null);
+        return m;
+//        }else{
+//            return new CommonResult(444,"没有对应记录,查询ID："+id,null);
+//        }
+    }
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        return serverPort;
+    }
+
+
+}
